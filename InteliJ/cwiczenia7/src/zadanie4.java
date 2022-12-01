28b5c41ded373ce7c41b7b8d6e3366cd82e45509
s28936 <115495407+s28936@users.noreply.github.com>
2022-12-01 18:49:17 +0100

    zadanie4 dokonczone

--- InteliJ/cwiczenia7/src/zadanie4.java
+++ InteliJ/cwiczenia7/src/zadanie4.java
@@ -1,19 +1,46 @@
 import java.util.Scanner;
 public class zadanie4 {
     public static void main(String[] args) {
-        System.out.println("Podaj liczbe calkowita x: ");
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.println("Podaj liczbe calkowita y: ");
-        int y = sc.nextInt();
-        System.out.println("Podaj liczbe calkowita z: ");
-        int z = sc.nextInt();
-        System.out.println("Zmienna x to: " +x);
-        System.out.println("Zmienna y to: " +y);
-        System.out.println("Zmienna z to: " +z);
-        wykres(x,y,z);
+        Scanner scanner = new Scanner(System.in);
+        //zakomentowana wersja na dowolna ilosc zmiennych
+        //System.out.print("Podaj ilosc kolumn w wykresie: ");
+        //int kolumny = scanner.nextInt();
+        int kolumny = 3;
+        int[] wartosci = new int[kolumny];
+        int zmienna = 0;
+        while( zmienna < kolumny ) {
+            System.out.printf("Podaj wartosc wartosc parametru na miejscu %d: ", zmienna);
+            wartosci[zmienna++] = scanner.nextInt();
+        }
+        rysujWykres( wartosci );
     }
-    public static void wykres(int x, int y, int z) {
-        //for
+    public static void rysujWykres(int[] wartosci) {
+        int rows = wartosci.length;
+        int maxValue = znajdzMax(wartosci);
+        String[] horizontalRow = new String[ maxValue + 1];
+        for(int rzedy = 0; rzedy <= maxValue; rzedy++) {
+            String wzorTegoRzedu = "";
+            int wykresTegoRzedu = maxValue - rzedy;
+            for(int kolumny = 0; kolumny < rows; kolumny++) {
+                if( wykresTegoRzedu < wartosci[kolumny] ) {
+                    wzorTegoRzedu += "* ";
+                } else {
+                    wzorTegoRzedu += "  ";
+                }
+            }
+            horizontalRow[rzedy] = wzorTegoRzedu;
+        }
+        for(String wzorek : horizontalRow) {
+            System.out.println(wzorek);
+        }
+    }
+    public static int znajdzMax(int[] wartosci) {
+        int max = 0;
+        for(int value : wartosci) {
+            if(max < value) {
+                max = value;
+            }
+        }
+        return max;
     }
 }
