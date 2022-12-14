5211ce8d07c1458492307d43ccd1fd43d3ad1157
s28936 <115495407+s28936@users.noreply.github.com>
2022-12-14 18:50:16 +0100

    dokonczenieCw9zad4

--- InteliJ/cwiczenia9/zadanie4.java
+++ InteliJ/cwiczenia9/zadanie4.java
@@ -3,19 +3,32 @@ import java.util.Arrays;
 public class zadanie4 {
     public static void main(String[] args) {
         int arr[] = { 5, 1, 1, 9, 3, 2, 6, 10 };
-        int toCheckValue = 3;
-        System.out.println("Array: "+ Arrays.toString(arr));
-        forCheck(arr, toCheckValue);
+        int klucz = 6;
+        System.out.println("Twoja tablica to: "+ Arrays.toString(arr));
+        forCheck(arr, klucz);
+        recursionCheck(arr, klucz, arr.length);
     }
-    private static void forCheck(int[] arr, int toCheckValue)
+    private static boolean forCheck(int[] arr, int klucz)
     {
         boolean test = false;
         for (int element : arr) {
-            if (element == toCheckValue) {
+            if (element == klucz) {
                 test = true;
                 break;
             }
         }
-        System.out.println("Is " + toCheckValue + " present in the array: " + test);
+        System.out.println("Twój klucz " + klucz + " znajduje sie w tablicy: " + test);
+        return test;
+    }
+    private static boolean recursionCheck(int[] arr, int klucz, int size){
+        if (size==0){
+            System.out.println("Blad - tablica ma rozmiar 0 i nie zawiera elementów");
+            return false;
+        }
+        if (arr[size-1]==klucz){
+            System.out.println("Twoj klucz "+ klucz +" znajduje sie w tablicy");
+            return true;
+        }
+        return recursionCheck(arr, klucz, size-1);
     }
 }
