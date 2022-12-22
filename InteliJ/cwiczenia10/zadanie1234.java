11c36f61bdefa6e2524ba366faa1515dee2feed1
s28936 <115495407+s28936@users.noreply.github.com>
2022-12-22 15:59:38 +0100

    poprawione

--- InteliJ/cwiczenia10/zadanie1234.java
+++ InteliJ/cwiczenia10/zadanie1234.java
@@ -5,40 +5,42 @@ import java.util.List;
 
 public class zadanie1234 {
     public static void main(String[] args) throws IOException {
-        //Zad1
+        System.out.println("Zadanie1: ");
         String test = "Podziel@mnie@na@czesci";
         String podzielnik = "@";
-        splitBy(test, podzielnik);
-       //Zad2
+        System.out.println("array: "+Arrays.toString(splitBy(test, podzielnik)));
+        System.out.println("Zadanie2: ");
         List<String> listOfStrings = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
         String line = reader.readLine();
-        // checking for end of file
         while (line != null) {
             listOfStrings.add(line);
             line = reader.readLine();
         }
-        // closing bufferreader object
         reader.close();
-        // storing the data in arraylist to array
         String[] array = listOfStrings.toArray(new String[0]);
-        // printing each line of file
-        // which is stored in array
         for (String str : array) {
             System.out.println(str);
         }
-        //zadanie3 - poprawic zad1 aby zwracalo array'a
+        System.out.println("Zadanie3: ");
         String[][] tablica2d = new String[10][3];
-        for (int i=0; i< tablica2d.length; i++){
-            for (int j=0; j<tablica2d[i].length; j++){
-                tablica2d[i][j]= splitBy(tablica2d[i],",");
-            }
+        for (int i=0; i< array.length; i++){
+            tablica2d[i] = splitBy(array[i],",");
         }
         System.out.println(Arrays.deepToString(tablica2d));
+        System.out.println("Zadanie4 w pliku data2");
+        File plik2 = new File("data2.txt");
+        plik2.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(plik2));
+        for (int x=0; x< array.length; x++){
+            writer.write(x+","+array[x]);
+            writer.newLine();
+        }
+        writer.close();
+
     }
     public static String[] splitBy(String line, String separator){
-        //poprawic aby dawalo arraya na wyjsciu
-        String[] tablica = java.util.Arrays.toString(line.split(separator));
-        return
+        String[] tablica = line.split(separator);
+        return tablica;
     }
 }
